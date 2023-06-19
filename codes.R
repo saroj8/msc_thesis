1280d0cc4a3e3f89023ffcb68ebc100612c27a44
Saroj <saroj8>
2023-06-19 21:14:58 +0545

    codes are written here

---
+++ codes.R
@@ -0,0 +1,6 @@
+# Modelling aboveground forest biomass using remote sensing in Nepal
+# 19th June 2023
+#Msc_thesis
+#Saroj Kandel
+#**********************************************************************************
+  

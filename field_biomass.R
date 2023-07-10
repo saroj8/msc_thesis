9238f34e0a26c94ab38c7d206eb64e04357f0b5c
Saroj <saroj8>
2023-07-10 21:06:41 +0545

    sum biomass

--- field_biomass.R
+++ field_biomass.R
@@ -25,16 +25,16 @@ abg_mean <- filter(abg_results, Plot.No == 8)
 abg_mean
 mean(abg_mean$abg)
 # try
-mean_values <- c()  # Empty vector to store the mean values
+sum_values <- c()
 
 for (plot_num in 1:51) {
   abg_mean <- filter(abg_results, Plot.No == plot_num)
-  mean_value <- mean(abg_mean$abg)
-  mean_values <- c(mean_values, mean_value)
+  sum_value <- sum(abg_mean$abg)
+  sum_values <- c(sum_values, sum_value)
 }
 
-mean_values_df <- data.frame(Plot.No = 1:51, Mean_abg = mean_values)
+sum_values_df <- data.frame(Plot.No = 1:51, Sum_abg = sum_values)
 # export this mean abg 
-file_path<- "F:\\2078 and 79\\MSc\\4th sem\\my_data\\mean.csv"
-write.csv(mean_values_df, file = file_path, row.names = FALSE)
+file_path<- "F:\\2078 and 79\\MSc\\4th sem\\my_data\\sum_abg.csv"
+write.csv(sum_values_df, file = file_path, row.names = FALSE)
 
